5ff758094c4c30fb491f852c43bd8459a0a54958
Florian Fehrenbacher <florian.fehrenbacher@sz.de>
2021-04-24 20:39:01 +0200

    small style fixes

--- src/components/district-card.tsx
+++ src/components/district-card.tsx
@@ -1,13 +1,14 @@
 import { CloseIcon, StarIcon } from '@chakra-ui/icons'
-import { AspectRatio, Box, Text } from '@chakra-ui/layout'
-import { IconButton, theme, VStack } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/layout'
+import { Flex, IconButton, theme } from '@chakra-ui/react'
 import { css } from '@emotion/react'
 import { useCurrentRKIData } from 'hooks/use-current-rki-data'
 import { useMyDistricts } from 'hooks/use-my-districts'
 import React from 'react'
 
-export const districtCardWidth = 352
-export const districtCardHeight = Math.floor(352 * (9 / 16))
+const ratio = 3 / 4
+export const districtCardWidth = 280
+export const districtCardHeight = Math.ceil(districtCardWidth * ratio)
 
 export function DistrictCard({ ags, ...props }: { ags: string }) {
   const { districts } = useCurrentRKIData()
@@ -17,24 +18,30 @@ export function DistrictCard({ ags, ...props }: { ags: string }) {
 
   if (!district) return <Box>Unbekannter Allgemeiner Gemeindeschlüssel: {ags}</Box>
   return (
-    <AspectRatio ratio={16 / 9} w={districtCardWidth} flexShrink={0} {...props}>
-      <VStack css={styles.container}>
-        <Text fontSize="2xl" whiteSpace="nowrap" textOverflow="ellipsis">
+    <Flex
+      justify="space-between"
+      css={styles.container}
+      w={districtCardWidth}
+      h={districtCardHeight}
+      align="start"
+      p="4"
+    >
+      <Box w="80%">
+        <Text fontSize="larger" whiteSpace="nowrap" textOverflow="ellipsis" css={styles.title}>
           {district.county}
         </Text>
         <Text>7-Tage-Inzidenz: {Math.round(district.weekIncidence)}</Text>
         <Text>Einwohner: {district.population}</Text>
         <Text>Neue Fälle: {district.delta.cases}</Text>
-        <IconButton
-          css={styles.saveButton}
-          as="a"
-          colorScheme="gray"
-          aria-label="Search database"
-          onClick={() => (myDistricts.includes(ags) ? removeDistrict(ags) : addDistrict(ags))}
-          icon={myDistricts.includes(ags) ? <CloseIcon /> : <StarIcon />}
-        />
-      </VStack>
-    </AspectRatio>
+      </Box>
+      <IconButton
+        as="a"
+        colorScheme="gray"
+        aria-label="Search database"
+        onClick={() => (myDistricts.includes(ags) ? removeDistrict(ags) : addDistrict(ags))}
+        icon={myDistricts.includes(ags) ? <CloseIcon /> : <StarIcon />}
+      />
+    </Flex>
   )
 }
 
@@ -43,10 +50,10 @@ const styles = {
     border: ${theme.borders['1px']} ${theme.colors.blackAlpha[400]};
     border-radius: ${theme.radii['2xl']};
     position: relative;
+    overflow: hidden;
   `,
-  saveButton: css`
-    position: absolute;
-    top: ${theme.space[2]};
-    right: ${theme.space[3]};
+  title: css`
+    overflow: hidden;
+    text-overflow: ellipsis;
   `,
 }
--- src/hooks/use-my-districts.ts
+++ src/hooks/use-my-districts.ts
@@ -1,3 +1,4 @@
+import { useToast } from '@chakra-ui/toast'
 import React from 'react'
 import useLocalStorageState, { createLocalStorageStateHook } from 'use-local-storage-state'
 import { AGS } from './use-current-rki-data'
@@ -7,13 +8,26 @@ const useDistricts = createLocalStorageStateHook<AGS[]>(myDistrictsKey, [])
 
 export function useMyDistricts() {
   const [myDistricts, setMyDistricts] = useDistricts()
+  const toast = useToast()
 
   const addDistrict = React.useCallback((district: AGS) => {
     setMyDistricts((prev) => [district, ...(prev ?? [])])
+    toast({
+      title: 'Erfolgreich hinzugefügt',
+      status: 'success',
+      duration: 3000,
+      isClosable: true,
+    })
   }, [])
 
   const removeDistrict = React.useCallback((district: AGS) => {
     setMyDistricts((prev) => prev?.filter((d) => d !== district))
+    toast({
+      title: 'Erfolgreich entfernt',
+      status: 'info',
+      duration: 3000,
+      isClosable: true,
+    })
   }, [])
 
   return { myDistricts: myDistricts ?? [], addDistrict, removeDistrict }
